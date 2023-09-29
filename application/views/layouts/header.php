098f01808d3554078cd0b5cce3a680590bb19830
henry <henry.aspeti@gmail.com>
2023-09-28 22:42:23 -0400

    add new font awwsome-icons

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/fontawesome-free-6.4.2-web/css/all.min.css">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
