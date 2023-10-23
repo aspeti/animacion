c9e2334fbac0f2acd92363f7f291df54d7366db9
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-23 15:16:47 -0400

    animacion_db modified

--- scripts/insert in tables.sql
+++ scripts/insert in tables.sql
@@ -1,16 +1,9 @@
+insert into rol (id_rol,nombre, descripcion, eliminado)values
+(1,"admin","administrador",0),
+(2,"cliente","cliente",0);
 
-insert into rol (nombre, descripcion, eliminado)values
-("admin","administrador",0),
-("user","usuario",0);
+insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
+(1,CURDATE(),0,"admin@gmail.com",MD5("admin"),'Admin','Apellido',1),
+(2,CURDATE(),0,"usuario@gmail.com",MD5("usuario"),'Lineth','Vallejos',2);
 
-select * from usuario;
-insert into usuario(fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
-(CURDATE(),0,"admin@gmail.com",MD5("admin"),'admin','apellidos',1);
-
-insert into rol (nombre, descripcion, eliminado)values
-("admin","administrador",0),
-("cliente","cliente",0);
-
-select * from categoria;
-insert into categoria(nombre,descripcion, eliminado)values
-("producto","descripcion",0);
+insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001')
