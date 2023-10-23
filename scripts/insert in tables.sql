90ccc75d3e4c05d36602dd9937fb0290c062c6cb
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-23 19:49:25 -0400

    add values to insert and add importe

--- scripts/insert in tables.sql
+++ scripts/insert in tables.sql
@@ -6,4 +6,10 @@ insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,ap
 (1,CURDATE(),0,"admin@gmail.com",MD5("admin"),'Admin','Apellido',1),
 (2,CURDATE(),0,"usuario@gmail.com",MD5("usuario"),'Lineth','Vallejos',2);
 
-insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001')
+insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001');
+
+insert into categoria (id_categoria,nombre, descripcion, eliminado)values
+(1,"Fijo","Servicios para Paquete",0),
+(2,"Adicional","Servicios de eleccion",0),
+(3,"Humor","Categoria de chistes",0),
+(4,"Tematica","Vestuario",0);
